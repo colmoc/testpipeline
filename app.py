779a1e32cc9fbbd723200d428a75b7f8dfe1e649
EC2 Default User <ec2-user@ip-172-31-2-224.ec2.internal>
2022-07-17 10:26:27 +0000

    update

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home - GREEN</h3>"
+    html = f"<h3>Sklearn Prediction Home - BLUE</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
